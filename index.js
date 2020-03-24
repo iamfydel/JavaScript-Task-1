3d7dcbbc37cffc6e863b832be8eefd491c8d860a
Hezekiah Feyisayo <hezekiahfeyisayo@gmail.com>
2020-03-24 11:54:24 +0100

    Initial Commit

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ console.log(name);
 
 let courses = ['HTML', 'CSS', 'JavaScript'];
 console.log(courses);
+
+//Display of all odd number from 1-200
+for ( let i = 1; i <= 200; i++) {
+if (i % 2 !== 0)
+console.log(i);
+}
